5b0f7e30e6fc1b25fd1393f94556ed24e629dc7a
Nick Guerra <nickg@maliciousgames.com>
2020-08-09 13:07:17 -0500

    added bar charts

--- src/api/index.js
+++ src/api/index.js
@@ -2,11 +2,17 @@ import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api';
 
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+  let changeableUrl = url;
+
+  if (country) {
+    changeableUrl = `${url}/countries/${country}`
+  }
+
   try {
     const {
-      data: {confirmed, recovered, deaths, lastUpdate},
-    } = await axios.get(url);
+      data: { confirmed, recovered, deaths, lastUpdate },
+    } = await axios.get(changeableUrl);
 
     return {
       confirmed,
@@ -14,12 +20,12 @@ export const fetchData = async () => {
       deaths,
       lastUpdate,
     };
-  } catch (error) {}
+  } catch (error) { console.log(error) }
 };
 
 export const fetchDailyData = async () => {
   try {
-    const {data} = await axios.get(`${url}/daily`);
+    const { data } = await axios.get(`${url}/daily`);
 
     const modifiedData = data.map((dailyData) => ({
       confirmed: dailyData.confirmed.total,
@@ -28,5 +34,16 @@ export const fetchDailyData = async () => {
     }));
 
     return modifiedData;
-  } catch (error) {}
+  } catch (error) { console.log(error) }
 };
+
+export const fetchCountries = async () => {
+  try {
+    const { data: { countries } } = await axios.get(`${url}/countries`)
+
+    return countries.map((country) => country.name)
+
+  } catch (error) {
+    console.log(error);
+  }
+}
